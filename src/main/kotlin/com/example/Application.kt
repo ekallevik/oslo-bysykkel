9a229ae42441d3cce4c6e49b615a390993bdf890
Even Kallevik <even.kallevik@gmail.com>
2023-06-26 10:56:50 +0200

    Add endpoint to serve station-overview

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,5 +1,12 @@
 package com.example
 
+import com.example.domain.StationOverviewParams
+import com.example.domain.StationsOverview
+import com.example.services.GbfsService
+import io.ktor.client.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.cache.*
+import io.ktor.client.request.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
@@ -9,7 +16,11 @@ import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.json.Json
 import org.slf4j.event.Level
+
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation as ClientContentNegotiation
+
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -24,17 +35,46 @@ fun Application.module() {
     install(ContentNegotiation) {
         json()
     }
+
     configureRouting()
 }
 
 fun Application.configureRouting() {
+
+    val baseUrl = "https://gbfs.urbansharing.com/oslobysykkel.no"
+
+    val client = HttpClient() {
+        install(HttpCache)
+        install(ClientContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
+        defaultRequest {
+            header("Client-Identifier", "ekallevik-bysykkel")
+        }
+    }
+
     routing {
         get("/") {
             call.respondText("Hello World!")
         }
 
-        get("/json") {
-            call.respond<Map<String, String>>(mapOf("hello" to "world"))
+        get("/stations") {
+
+            val parameters = StationOverviewParams.parse(
+                call.request.queryParameters["excludeMissingStatus"]?.toBoolean(),
+                call.request.queryParameters["excludeNonInstalled"]?.toBoolean(),
+                call.request.queryParameters["excludeEmpty"]?.toBoolean(),
+                call.request.queryParameters["excludeFull"]?.toBoolean(),
+            )
+
+            val stations = GbfsService.getBicycleStations(client, baseUrl)
+            val response = StationsOverview.valueOf(stations, parameters)
+
+            call.respond(response)
         }
     }
 }
--- src/test/kotlin/com/example/ApplicationTest.kt
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -5,7 +5,6 @@ import io.ktor.client.statement.*
 import io.ktor.server.testing.*
 import kotlin.test.*
 import io.ktor.http.*
-import com.example.plugins.*
 
 class ApplicationTest {
     @Test
